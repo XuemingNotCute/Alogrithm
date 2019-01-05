bfa0a92d127ebcc545456d741b8dda833dff2b2c
XuemingNotCuteAtAll <43215381+XuemingNotCute@users.noreply.github.com>
2019-01-05 17:37:44 +0800

    Update quick_sort.py

--- quick_sort/quick_sort.py
+++ quick_sort/quick_sort.py
@@ -1,1 +1,11 @@
-//
+def quick_sort(array):
+    # pivot 就是基准值
+    if len(array) < 2:
+        return array
+    else:
+        pivot = array[0]
+        greater = [i for i in array[1:] if i >= pivot]
+        less = [i for i in array[1:] if i < pivot]
+        return quick_sort(less) + [pivot] + quick_sort(greater)
+
+
