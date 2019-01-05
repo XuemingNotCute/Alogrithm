e4423087609d7c9675ea5d2ee3c5bb29f63076f0
XuemingNotCuteAtAll <43215381+XuemingNotCute@users.noreply.github.com>
2019-01-05 17:40:40 +0800

    Update test.py

--- selection_sort/test.py
+++ selection_sort/test.py
@@ -3,20 +3,20 @@ import selection_sort
 
 def test1():
     input = [5, 8, 9, 0, 3, 5, 2, 1]
-    output = selectionSort.selection_sort(input)
+    output = selection_sort.selection_sort(input)
     expected = [0, 1, 2, 3, 5, 5, 8, 9]
     assert output == expected, output
 
 
 def test2():
-    input = [100, 8, 9, 50, 45, 34, 12, 7, 1000]
-    output = selectionSort.selection_sort(input)
-    expected = [7, 8, 9, 12, 34, 45, 50, 100, 1000]
+    input = [100, 8, 9, 50, 45, 34, 12, 7, 10000]
+    output = selection_sort.selection_sort(input)
+    expected = [7, 8, 9, 12, 34, 45, 50, 100, 10000]
     assert output == expected, output
 
 
 def test3():
     input = [5, 3, 6, 2, 10]
-    output = selectionSort.selection_sort(input)
+    output = selection_sort.selection_sort(input)
     expected = [2, 3, 5, 6, 10]
     assert output == expected, output
