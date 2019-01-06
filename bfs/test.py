69c9ea6f2772d8a1d5bd1bb101f08d1cf414d0d9
XuemingNotCuteAtAll <43215381+XuemingNotCute@users.noreply.github.com>
2019-01-06 18:12:03 +0800

    Update test.py

--- bfs/test.py
+++ bfs/test.py
@@ -1,1 +1,25 @@
 
+import search
+
+
+# 输入名字，检查某人的好友里有无芒果营销商
+def test1():
+    input = 'you'
+    output = search.search(input)
+    expected = True
+    assert output == expected, output
+
+
+def test2():
+    input = 'thom'
+    output = search.search(input)
+    expected = False
+    assert output == expected, output
+
+
+def test3():
+    input = 'claire'
+    output = search.search(input)
+    expected = True
+    assert output == expected, output
+
